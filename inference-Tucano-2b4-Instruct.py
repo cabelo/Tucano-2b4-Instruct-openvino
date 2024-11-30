003649c0798e492b5c72b30378008ff35e48d854
Alessandro de Oliveira Faria (A.K.A.CABELO) <cabelo@opensuse.org>
2024-11-30 19:18:02 -0300

    Create inference-Tucano-2b4-Instruct.py

---
+++ inference-Tucano-2b4-Instruct.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer
+from optimum.intel.openvino import OVModelForCausalLM
+
+model_id = "cabelo/Tucano-2b4-Instruct-fp16-ov"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = OVModelForCausalLM.from_pretrained(model_id)
+
+inputs = tokenizer("What is OpenVINO?", return_tensors="pt")
+
+outputs = model.generate(**inputs, max_length=200)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
