e1900cacce7c39e631ae225b102abfd8a1ae92f4
Alessandro de Oliveira Faria (A.K.A.CABELO) <cabelo@opensuse.org>
2024-12-02 00:51:11 -0300

    Update inference-Tucano-2b4-Instruct.py

--- inference-Tucano-2b4-Instruct.py
+++ inference-Tucano-2b4-Instruct.py
@@ -5,7 +5,7 @@ model_id = "cabelo/Tucano-2b4-Instruct-fp16-ov"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = OVModelForCausalLM.from_pretrained(model_id)
 
-inputs = tokenizer("Qual cidade é a capital do estado do Rio Grande do Sul?", return_tensors="pt")
+inputs = tokenizer("<instruction>Qual cidade é a capital do estado do Rio Grande do Sul?</instruction>", return_tensors="pt")
 
 outputs = model.generate(**inputs, max_length=200)
 text = tokenizer.batch_decode(outputs)[0]
