247be5497bb3326e6cb755164d99fe44432614a5
Alessandro de Oliveira Faria (A.K.A.CABELO) <cabelo@opensuse.org>
2024-12-01 23:52:41 -0300

    Update inference-Tucano-2b4-Instruct.py

--- inference-Tucano-2b4-Instruct.py
+++ inference-Tucano-2b4-Instruct.py
@@ -5,7 +5,7 @@ model_id = "cabelo/Tucano-2b4-Instruct-fp16-ov"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = OVModelForCausalLM.from_pretrained(model_id)
 
-inputs = tokenizer("o que é Carnaval?", return_tensors="pt")
+inputs = tokenizer("Como é Carnaval no Brasil?", return_tensors="pt")
 
 outputs = model.generate(**inputs, max_length=200)
 text = tokenizer.batch_decode(outputs)[0]
